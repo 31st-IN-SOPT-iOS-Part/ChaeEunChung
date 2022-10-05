ebdbae8f12210816f4fab2c32b8a3a365848badf
정채은 <cce523@hanmail.net>
2022-10-06 00:27:58 +0900

    과제1 구현

---
+++ IN_SOPT_First_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IN_SOPT_First_Assignment
+//
+//  Created by 정채은 on 2022/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
